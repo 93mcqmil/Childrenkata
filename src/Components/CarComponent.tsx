4602507fa0928b5ac80ddd2c2a95e37a6848fb8c
93mcqmil <mcqueen_1993@hotmail.com>
2024-03-21 18:04:10 +0100

    rest of the car component

--- src/Components/CarComponent.tsx
+++ src/Components/CarComponent.tsx
@@ -4,22 +4,26 @@ type Car = {
   id: number;
   description: string;
   model: string;
+  complete: boolean;
 };
 
 const CarComponent = () => {
   const CarLists: Car[] = [
     {
       id: 1,
+      complete: true,
       description: "Smooth design, sporty feel.",
       model: "Toyota Camry",
     },
     {
       id: 2,
+      complete: true,
       description: "Tough, reliable, spacious.",
       model: "Jeep Wrangler",
     },
     {
       id: 3,
+      complete: true,
       description: "Eco-friendly, fuel-efficient.",
       model: "Toyota Prius",
     },
@@ -31,13 +35,28 @@ const CarComponent = () => {
     <div>
       <h2>CarLists</h2>
       <ul>
-        {Cars.map((c: Car) => {
-          return (
-            <li key={c.id}>
-              {c.model}, {c.description}
-            </li>
-          );
-        })}
+        {Cars.map((c: Car) => (
+          <li key={c.id}>
+            {c.complete ? (
+              <span
+                onClick={() => {
+                  setCar((prevCars) =>
+                    prevCars.map((car) => {
+                      if (car.id === c.id) {
+                        return { ...c, complete: !c.complete }; // Uppdaterar complete-värdet till det motsatta av nuvarande värde
+                      } else {
+                        return car;
+                      }
+                    })
+                  );
+                }}
+              >
+                {c.complete ? "✅" : "❌"}{" "}
+                {/* Visar olika ikon beroende på complete-värdet */}
+              </span>
+            ) : null}
+          </li>
+        ))}
       </ul>
     </div>
   );
